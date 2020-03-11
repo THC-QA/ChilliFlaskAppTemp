d58d57d110b40b2b026aaff7e0b127fb97dd6f01
THC-QA <Tristan.Clapp@academytrainee.com>
2020-03-11 14:14:42 +0000

    requirements created

--- app.py
+++ app.py
@@ -57,25 +57,34 @@ def about():
 def account():
     r_form = recipe_form()
     i_form = ingredient_form()
-    if r_form.validate_on_submit() and request.method == "POST":
-        recipe_name = r_form.recipe_name.data
-        recipe_method = r_form.recipe_method.data
-        flash("Thank you for your submission of {} sauce.".format(r_form.recipe_name.data))
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO recipes(recipe_name, recipe_method) VALUES (%s, %s)", (recipe_name, recipe_method))
-        mysql.connection.commit()
-        cur.close()
-        return redirect("/browse")
-    elif i_form.validate_on_submit() and request.method == "POST":
-        flash("Thank you for your submission of {} ingredient.".format(i_form.ingredient_name.data))
-        ingredient_name = i_form.ingredient_name.data
-        ingredient_type = i_form.ingredient_type.data
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO ingredients(ingredient_name, ingredient_type) VALUES (%s, %s)", (ingredient_name, ingredient_type))
-        mysql.connection.commit()
-        cur.close()
-        return redirect("/browse")
-    return render_template('account.html', title = 'Account', recipe_form = r_form, ingredient_form = i_form)
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT recipe_name FROM recipes")
+    r_names = cur.fetchall()
+    mysql.connection.commit()
+    cur.execute("SELECT ingredient_name FROM ingredients")
+    i_names = cur.fetchall()
+    mysql.connection.commit()
+    cur.close()
+    if request.method == "POST":
+        if r_form.validate_on_submit():
+            recipe_name = r_form.recipe_name.data
+            recipe_method = r_form.recipe_method.data
+            flash("Thank you for your submission of {} sauce.".format(r_form.recipe_name.data))
+            cur = mysql.connection.cursor()
+            cur.execute("INSERT INTO recipes(recipe_name, recipe_method) VALUES (%s, %s)", (recipe_name, recipe_method))
+            mysql.connection.commit()
+            cur.close()
+            return redirect("/browse")
+        elif i_form.validate_on_submit():
+            flash("Thank you for your submission of {} ingredient.".format(i_form.ingredient_name.data))
+            ingredient_name = i_form.ingredient_name.data
+            ingredient_type = i_form.ingredient_type.data
+            cur = mysql.connection.cursor()
+            cur.execute("INSERT INTO ingredients(ingredient_name, ingredient_type) VALUES (%s, %s)", (ingredient_name, ingredient_type))
+            mysql.connection.commit()
+            cur.close()
+            return redirect("/browse")
+    return render_template('account.html', title = 'Account', recipe_form = r_form, ingredient_form = i_form, recipes = r_names, ingredients = i_names)
 
 @app.route('/browse')
 def browse():
@@ -94,7 +103,7 @@ def browse():
     cur.close()
     return render_template('browse.html', title = 'Browse All', recipes = recipes, index = index)
 
-@app.route('/admin')
+@app.route('/admin', methods = ["GET", "POST"])
 def admin():
     cur = mysql.connection.cursor()
     cur.execute("SELECT recipe_name FROM recipes")
@@ -106,6 +115,34 @@ def admin():
     cur.close()
     u_form = update_form(r_names, i_names)
     return render_template('admin.html', title = "ADMIN", update_form = u_form)
+
+@app.route('/mvp', methods = ["GET", "POST"])
+def minimum():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT recipe_name FROM recipes")
+    r_names = cur.fetchall()
+    mysql.connection.commit()
+    cur.close()
+    if request.method == "POST":
+        details = request.form
+        if "new_name" in details:
+            recipe_name = details["recipe_name"]
+            new_name = details["new_name"]
+            cur = mysql.connection.cursor()
+            cur.execute("UPDATE recipes SET recipe_name = (%s) WHERE recipe_name = (%s);", (new_name, recipe_name))
+            mysql.connection.commit()
+            cur.close()
+            return redirect("/browse")
+        else:
+            recipe_name = details["recipe_name"]
+            cur = mysql.connection.cursor()
+            cur.execute("DELETE FROM recipe_ingredients WHERE recipe_name = (%s);", [recipe_name])
+            mysql.connection.commit()
+            cur.execute("DELETE FROM recipes WHERE recipe_name = (%s);", [recipe_name])
+            mysql.connection.commit()
+            cur.close()
+            return redirect("/browse")
+    return render_template('mvp.html', title = "MINIMUM VIABILITY", recipes = r_names)
 
 if __name__ == '__main__':
     app.run('0.0.0.0', debug = True)
