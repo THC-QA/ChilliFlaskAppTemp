80f15c94f64b78052ae06e958ffd1b228f841281
THC-QA <Tristan.Clapp@academytrainee.com>
2020-03-16 16:53:29 +0000

    quotation marks

--- scripts/make_service.sh
+++ scripts/make_service.sh
@@ -1,4 +1,4 @@
-sudo cp /var/lib/jenkins/workspace/FlaskApp Example Pipeline/service/ChilliApp.service /etc/systemd/system
+sudo cp "/var/lib/jenkins/workspace/FlaskApp Example Pipeline/service/ChilliApp.service" /etc/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl enable ChilliApp.service
 sudo systemctl start ChilliApp.service
