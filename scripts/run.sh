6b6b87839023bf5cd04581529e3f0d99f240f692
THC-QA <Tristan.Clapp@academytrainee.com>
2020-03-17 14:31:33 +0000

    gunicorn test3

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,4 +4,4 @@ source ~/.bashrc
 
 python3 "/var/lib/jenkins/workspace/FlaskApp Example Pipeline/app.py"
 
-"/var/lib/jenkins/workspace/FlaskApp Example Pipeline/venv/bin/gunicorn" --workers=4 --bind=0.0.0.0:5001 app:app
+/var/lib/jenkins/workspace/"FlaskApp Example Pipeline"/venv/bin/gunicorn --workers=4 --bind=0.0.0.0:5001 app:app
