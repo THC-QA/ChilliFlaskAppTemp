4f583cecd952a5dec61de30c1196c52eebb83d93
THC-QA <Tristan.Clapp@academytrainee.com>
2020-03-17 11:29:24 +0000

    service issues

--- scripts/make_service.sh
+++ scripts/make_service.sh
@@ -1,3 +1,4 @@
+sudo systemctl stop ChilliApp.service
 sudo cp "/var/lib/jenkins/workspace/FlaskApp Example Pipeline/service/ChilliApp.service" /etc/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl enable ChilliApp.service
