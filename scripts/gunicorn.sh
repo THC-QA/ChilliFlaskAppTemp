9bfbd342788a843bcc56981d34d83fba104bfb4f
THC-QA <Tristan.Clapp@academytrainee.com>
2020-03-19 10:23:57 +0000

    port change

--- scripts/gunicorn.sh
+++ scripts/gunicorn.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-"/var/lib/jenkins/workspace/FlaskApp Example Pipeline/venv/bin/gunicorn" --workers=6 --bind=0.0.0.0:5001 app:app
+"/var/lib/jenkins/workspace/FlaskApp Example Pipeline/venv/bin/gunicorn" --workers=6 --bind=0.0.0.0:80 app:app
